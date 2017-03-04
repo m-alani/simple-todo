2dbb4061e031c80e92820da3ade70008449b8cf4
Marwan Alani <marwan@alani.ca>
2017-03-04 16:11:28 -0500

    Initial Commit

---
+++ Simple To Do/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple To Do
+//
+//  Created by Marwan Alani on 2017-03-04.
+//  Copyright © 2017 Marwan Alani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
