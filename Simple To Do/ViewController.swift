923dc16d8281ab37c4ab83ba83e1e9081826d1a6
Marwan Alani <marwan@alani.ca>
2017-03-04 19:02:52 -0500

    Added support for CoreData to save & load items

--- Simple To Do/ViewController.swift
+++ Simple To Do/ViewController.swift
@@ -7,28 +7,30 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var todoItems = [String]()
+    var todoItems = [NSManagedObject]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         self.title = "To Do List!"
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        self.loadList()
     }
     
     @IBAction func addItem(_ sender: Any) {
         let alert = UIAlertController(title: "New Item", message: "Add something to do", preferredStyle: .alert)
         
         let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
-            guard let textField = alert.textFields?.first,
-            let nameToSave = textField.text else {
-                return
-            }
-            self.todoItems.append(nameToSave)
+            guard let textField = alert.textFields?.first ,
+                let todoItem = textField.text else {
+                    return
+                }
+            self.saveList(todoItem: todoItem)
             self.tableView.reloadData()
         }
         
@@ -40,19 +42,47 @@ class ViewController: UIViewController {
         
         present(alert, animated: true)
     }
-
+    
+    func saveList(todoItem: String) {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let entity = NSEntityDescription.entity(forEntityName: "Items", in: managedContext)!
+        let item = NSManagedObject(entity: entity, insertInto: managedContext)
+        item.setValue(todoItem, forKeyPath: "todo")
+        do {
+            try managedContext.save()
+            todoItems.append(item)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    func loadList() {
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        let managedContext = appDelegate.persistentContainer.viewContext
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "Items")
+        do {
+            todoItems = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
 }
 
 // MARK: - UITableViewDataSource
 extension ViewController: UITableViewDataSource {
-    
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return todoItems.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-            let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-            cell.textLabel?.text = todoItems[indexPath.row]
-            return cell
+        let item = todoItems[indexPath.row]
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = item.value(forKeyPath: "name") as? String
+        return cell
     }
 }
